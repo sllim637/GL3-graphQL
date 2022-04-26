e45272df69c0f06664c4a0ff62474495252a16f6
slim <slim.hammami1977@gmail.com>
2022-04-26 16:18:58 +0200

    all tasks done

---
+++ data/db.mjs
@@ -0,0 +1,37 @@
+export const db = {
+    todos: [
+      {
+        id: 1,
+        name: "study",
+        content: "premiere lecture pour les cours",
+        status: "IN_PROGRESS",
+        userId: 1
+      },
+      {
+        id: 2,
+        name: "jouer",
+        content: "jouer valorant",
+        status: "WAITING",
+        userId: 2
+      },
+      {
+        id: 3,
+        name: "preparer",
+        content: "preparer couscous .",
+        status: "DONE",
+        userId: 1
+      }
+    ],
+    users: [
+      {
+        id: 1,
+        name: 'slim',
+        email: 'slim.hammami.sl.ha@gmail.com'
+      },
+      {
+        id: 2,
+        name: 'mohamed',
+        email: 'mouhasahss@gmail.com'
+      }
+    ]
+  };
